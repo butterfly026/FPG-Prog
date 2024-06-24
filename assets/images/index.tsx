87fccac1cfa7851df66d77337564ac84dc4eeed4
butterfly026 <butterfly026@github.com>
2024-06-24 04:42:58 -0500

    images

---
+++ assets/images/index.tsx
@@ -0,0 +1,54 @@
+export const Home = require('./home.png');
+export const HomeActive = require('./home_active.png');
+
+export const SponsorService = require('./sponsor.png');
+export const SponsorServiceActive = require('./sponsor_active.png');
+
+export const Guide = require('./icon_guide.png');
+export const GuideActive = require('./icon_guide_active.png');
+
+export const CustomerService = require('./csonline.png');
+export const CustomerServiceActive = require('./csonline_active.png');
+
+export const Profile = require('./profile.png');
+export const ProfileActive = require('./profile_active.png');
+
+export const LoadingGif = require('./loading.gif');
+// 聊天室中奖公告背图
+export const ChatRewardBg = require('./lottorywin_bg.png');
+// 聊天室中奖公告钱币gif
+export const ChatRewardCoinIcon = require('./winPublicMessage.gif');
+// 投注区筹码
+export const ChipGroup = require('./chouma-bg.png');
+// 彩票大厅自由版Tab背图
+export const GameHallTabBg = require('./game-select.png');
+// 彩票大厅自由版今日输赢
+export const GameListtrsy = require('./gameListjrsy.png');
+// 彩票大厅自由版历史开奖
+export const GameListlskj = require('./gameListlskj.png');
+// 彩票大厅自由版优惠活动
+export const GameListyhhd = require('./gameListyhhd.png');
+
+export const RedBall3d = require('./ball/red3.png');
+export const BlueBall3d = require('./ball/blue3.png');
+export const GreenBall3d = require('./ball/green3.png');
+
+export const RedBall = require('./ball/red.png');
+export const BlueBall = require('./ball/blue.png');
+export const GreenBall = require('./ball/green.png');
+
+export const RedBall65 = require('./ball/red65.png');
+export const BlueBall65 = require('./ball/blue65.png');
+export const GreenBall65 = require('./ball/green65.png');
+
+export const GrabRedEnvelopBg = require('./unGrab.png');
+
+export const OpenRedEnvelopButton = require('./openRedBag.png');
+export const RedEnvelopDetailBg = require('./redBagDetail.png');
+
+export const SaoLeiIcon = require('./slxxhdpi.png');
+
+export const PublicIconGroup = require('./publicIcon.png');
+
+// 聊天室背图
+export const ChatroomBg = require('./chatroom-bg.jpg');
---
+++ assets/images/tabbar/index.ts
@@ -0,0 +1,43 @@
+import FaUserO from './user-o.svg';
+import FaBuildingO from './building-o.svg';
+import FaCommentingO from './commenting-o.svg';
+import FaCalendarCheckO from './calendar-check-o.svg';
+import FaCreditCard from './credit-card.svg';
+import FaEnvelopOpenO from './envelope-open-o.svg';
+import FaGift from './gift.svg';
+import FaHeadphones from './headphones.svg';
+import FaHome from './home.svg';
+import FaListAlt from './list-alt.svg';
+import FaPencilSquareO from './pencil-square-o.svg';
+import FaRandom from './random.svg';
+import FaShirtsInBulk from './shirtsinbulk.svg';
+import FaTasks from './tasks.svg';
+import FaTrophy from './trophy.svg';
+import FaUnlockAlt from './unlock-alt.svg';
+import FaTachoMeter from './tachometer.svg';
+import FaHandRock from './hand-rock.svg';
+import FaShoppingCart from './shopping-cart.svg';
+import FaMapO from './map-o.svg';
+
+export {
+  FaUserO,
+  FaBuildingO,
+  FaCommentingO,
+  FaCalendarCheckO,
+  FaCreditCard,
+  FaEnvelopOpenO,
+  FaGift,
+  FaHeadphones,
+  FaHome,
+  FaListAlt,
+  FaPencilSquareO,
+  FaRandom,
+  FaShirtsInBulk,
+  FaTasks,
+  FaTrophy,
+  FaUnlockAlt,
+  FaTachoMeter,
+  FaHandRock,
+  FaShoppingCart,
+  FaMapO,
+};
